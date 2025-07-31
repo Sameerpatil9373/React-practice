4d396d4ac38463318a4bf89bee5f9912dd78e1d9
Sameerpatil9373 <sameerpatil9373@gmail.com>
2025-07-31 18:59:45 +0530

    update

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Pract from "./test"
+function App() {
+ const username ="testing react"
+  return (
+    <>
+  <Pract/>
+  <h1>chai aur react {username}</h1>
+  <p>test para</p>
+  </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/test.jsx
@@ -0,0 +1,7 @@
+function Pract(){
+    return (
+        <h3>this is test</h3>
+    )
+}
+
+export default Pract
